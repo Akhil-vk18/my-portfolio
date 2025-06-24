a4e585f0f093c95f592ff9eb72fe09c0deb0882f
Akhil santhosh <akhilsanthosh043@gmail.com>
2025-06-25 03:10:56 +0530

    contact section completed and theme toggle part partialy completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -7,8 +7,17 @@ import './App.css';
 
 
 function App(){
+  const [darkmode,setDarkMode]=useState(false);
+  const changeTheme =()=>{
+    setDarkMode(!darkmode);
+  };
   return (
-    <div>
+    <div className={darkmode?'app dark':'app'}>
+      <div className='toggle-Wrapper' onClick={changeTheme}>
+        <div className={'toggle-circle ${darkmode?"Dark":""}'}>
+        {darkmode?"☀️":"🌙"}
+        </div>
+      </div>
       <Header />
       <Projects />
       <Contact />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,57 @@
 import React from "react";
+import {
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+  FaMapMarkedAlt,
+} from "react-icons/fa";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <section style={{padding:'2rem',textAlign:"center"}}>
-        <h2>Contact Me</h2>
-        <p>Email: <a href="mailto:akhilsanthosh043@gmail.com">akhilsanthosh043@gmail.com</a></p>
-        <p>GitHub:<a href="https://github.com/Akhil-vk18" target="_blank" rel="noopener noreferrer">Akhil-vk18</a></p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/akhil-santhosh-1b381228a" target="_blank" rel="noopener noreferrer">Akhil Santhosh</a></p>
+    <section style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>Contact Me</h2>
+      <div className="contact-icons">
+        <div className="icon-item">
+          <a href="mailto:akhilsanthosh043@gmail.com" className="email">
+            <span className=" icon">
+              <FaEnvelope />
+            </span>
+            <span className="label">Email</span>
+          </a>
+        </div>
+        <div className="icon-item">
+          <a
+            href="https://github.com/Akhil-vk18"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github"
+          >
+            <span className="icon">
+              <FaGithub />
+            </span>
+            <span className="label">Github</span>
+          </a>
+        </div>
+        <div className="icon-item">
+          <a
+            href="https://www.linkedin.com/in/akhil-santhosh-1b381228a"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedin"
+          >
+            <span className="icon">
+              <FaLinkedin />
+            </span>
+            <span className="label">LinkedIn</span>
+          </a>
+        </div>
+      </div>
+      <p className="location">
+          <FaMapMarkedAlt style={{ marginRight: "8px" }} />
+          Kerala ,India
+        </p>
     </section>
-    
   );
 }
 export default Contact;
